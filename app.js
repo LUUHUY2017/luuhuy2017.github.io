5857cdef37cb8b6383166922f4c238720ebd84fc
LUUHUY2017 <nhathaibinh2012@gmail.com>
2019-06-02 22:46:11 +0700

    thay doi lon

--- app.js
+++ app.js
@@ -11,29 +11,29 @@ var template = "<article>\n\
 	</ul>\n\
 </article>";
 var content = '';
-for(var i=0; i<games.length; i++) {
-	var entry = template.replace(/POS/g,(i+1))
-		.replace(/SLUG/g,games[i].slug)
-		.replace(/NAME/g,games[i].name)
-		.replace(/AUTHOR/g,games[i].author)
-		.replace(/TWITTER/g,games[i].twitter)
-		.replace(/WEBSITE/g,games[i].website)
-		.replace(/GITHUB/g,games[i].github);
-	entry = entry.replace('<a href=\'http:///\'></a>','-');
+for (var i = 0; i < games.length; i++) {
+	var entry = template.replace(/POS/g, (i + 1))
+		.replace(/SLUG/g, games[i].slug)
+		.replace(/NAME/g, games[i].name)
+		.replace(/AUTHOR/g, games[i].author)
+		.replace(/TWITTER/g, games[i].twitter)
+		.replace(/WEBSITE/g, games[i].website)
+		.replace(/GITHUB/g, games[i].github);
+	entry = entry.replace('<a href=\'http:///\'></a>', '-');
 	content += entry;
 };
 document.getElementById('content').innerHTML = content;
 
-// Registering Service Worker
-if('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('sw.js');
-};
+// // Registering Service Worker
+// if('serviceWorker' in navigator) {
+// 	navigator.serviceWorker.register('sw.js');
+// };
 
 // Requesting permission for Notifications after clicking on the button
 var button = document.getElementById("notifications");
-button.addEventListener('click', function(e) {
-	Notification.requestPermission().then(function(result) {
-		if(result === 'granted') {
+button.addEventListener('click', function (e) {
+	Notification.requestPermission().then(function (result) {
+		if (result === 'granted') {
 			randomNotification();
 		}
 	});
@@ -41,10 +41,10 @@ button.addEventListener('click', function(e) {
 
 // Setting up random Notification
 function randomNotification() {
-	var randomItem = Math.floor(Math.random()*games.length);
+	var randomItem = Math.floor(Math.random() * games.length);
 	var notifTitle = games[randomItem].name;
-	var notifBody = 'Created by '+games[randomItem].author+'.';
-	var notifImg = 'data/img/'+games[randomItem].slug+'.jpg';
+	var notifBody = 'Created by ' + games[randomItem].author + '.';
+	var notifImg = 'data/img/' + games[randomItem].slug + '.jpg';
 	var options = {
 		body: notifBody,
 		icon: notifImg
@@ -55,81 +55,94 @@ function randomNotification() {
 
 // Progressive loading images
 var imagesToLoad = document.querySelectorAll('img[data-src]');
-var loadImages = function(image) {
+var loadImages = function (image) {
 	image.setAttribute('src', image.getAttribute('data-src'));
-	image.onload = function() {
+	image.onload = function () {
 		image.removeAttribute('data-src');
 	};
 };
-if('IntersectionObserver' in window) {
-	var observer = new IntersectionObserver(function(items, observer) {
-		items.forEach(function(item) {
-			if(item.isIntersecting) {
+if ('IntersectionObserver' in window) {
+	var observer = new IntersectionObserver(function (items, observer) {
+		items.forEach(function (item) {
+			if (item.isIntersecting) {
 				loadImages(item.target);
 				observer.unobserve(item.target);
 			}
 		});
 	});
-	imagesToLoad.forEach(function(img) {
+	imagesToLoad.forEach(function (img) {
 		observer.observe(img);
 	});
 }
 else {
-	imagesToLoad.forEach(function(img) {
+	imagesToLoad.forEach(function (img) {
 		loadImages(img);
 	});
 }
 
 
-
-const images = ['fox1','fox2','fox3','fox4'];
-const imgElem = document.querySelector('img');
-
-function randomValueFromArray(array) {
-  let randomNo =  Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
-
-setInterval(function() {
-  let randomChoice = randomValueFromArray(images);
-  imgElem.src = 'images/' + randomChoice + '.jpg';
-}, 2000)
-
-// Register service worker to control making site work offline
-
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker
-           .register('sw.js')
-           .then(function() { console.log('Service Worker Registered'); });
+if ('serviceWorker' in navigator) {
+	navigator.serviceWorker
+		.register('sw.js')
+		.then(function () { console.log('Service Worker Registered'); });
 }
 
 // Code to handle install prompt on desktop
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+// addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+	// Prevent Chrome 67 and earlier from automatically showing the prompt
+	e.preventDefault();
+	// Stash the event so it can be triggered later.
+	deferredPrompt = e;
+	// // Update UI to notify the user they can add to home screen
+	// addBtn.style.display = 'block';
 
-  addBtn.addEventListener('click', (e) => {
-    // hide our user interface that shows our A2HS button
-    addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
-        } else {
-          console.log('User dismissed the A2HS prompt');
-        }
-        deferredPrompt = null;
-      });
-  });
+	// addBtn.addEventListener('click', (e) => {
+	//   // hide our user interface that shows our A2HS button
+	//   addBtn.style.display = 'none';
+	//   // Show the prompt
+	//   deferredPrompt.prompt();
+	//   // Wait for the user to respond to the prompt
+	//   deferredPrompt.userChoice.then((choiceResult) => {
+	// 	  if (choiceResult.outcome === 'accepted') {
+	// 		console.log('User accepted the A2HS prompt');
+	// 	  } else {
+	// 		console.log('User dismissed the A2HS prompt');
+	// 	  }
+	// 	  deferredPrompt = null;
+	// 	});
+	// });
+
+	showAddToHomeScreen();
 });
+
+function showAddToHomeScreen() {
+
+	var a2hsBtn = document.querySelector(".add-button");
+
+	a2hsBtn.style.display = "block";
+
+	a2hsBtn.addEventListener("click", addToHomeScreen);
+
+}
+function addToHomeScreen() {
+	var a2hsBtn = document.querySelector(".add-button");  // hide our user interface that shows our A2HS button
+	a2hsBtn.style.display = 'none';  // Show the prompt
+	deferredPrompt.prompt();  // Wait for the user to respond to the prompt
+	deferredPrompt.userChoice
+		.then(function (choiceResult) {
+
+			if (choiceResult.outcome === 'accepted') {
+				console.log('User accepted the A2HS prompt');
+			} else {
+				console.log('User dismissed the A2HS prompt');
+			}
+
+			deferredPrompt = null;
+
+		});
+}

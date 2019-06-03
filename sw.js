24942da624c4ddff3f5d927eab0bc92e75684780
LUUHUY2017 <nhathaibinh2012@gmail.com>
2019-06-03 08:48:02 +0700

    ui bán han

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('video-store').then(function(cache) {
-     return cache.addAll([
-       '',
-       'index.html',
-       'index.js',
-       'style.css',
-       'images/fox1.jpg',
-       'images/fox2.jpg',
-       'images/fox3.jpg',
-       'images/fox4.jpg'
-     ]);
-   })
- );
-});
+// self.addEventListener('install', function(e) {
+//  e.waitUntil(
+//    caches.open('video-store').then(function(cache) {
+//      return cache.addAll([
+//        '',
+//        'index.html',
+//        'index.js',
+//        'style.css',
+//        'images/fox1.jpg',
+//        'images/fox2.jpg',
+//        'images/fox3.jpg',
+//        'images/fox4.jpg'
+//      ]);
+//    })
+//  );
+// });
 
 self.addEventListener('fetch', function(e) {
   console.log(e.request.url);

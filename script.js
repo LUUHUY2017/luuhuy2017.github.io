0929d498d9a34e59973d9841255c46ce8d07e7f0
Lưu Đức Huy <33080759+LUUHUY2017@users.noreply.github.com>
2023-07-25 15:39:32 +0700

    Update script.js
    ss

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 getIndex();
 setInterval(getIndex, 1000);
+setInterval(GetCoinGood, 1000);
+
 document.getElementById("myBtn").addEventListener("click", onShowHideModal);
 document.getElementById("confirm").addEventListener("click", onConfrim);
-
 
 
 
@@ -29,11 +30,9 @@ async function getIndex() {
     document.getElementById("EventTime").innerHTML = dateToDDMMYYYHHMMSS(new Date()) || "";
     getIndex1();
 }
-
-
 async function getIndex1() {
     var symbol = localStorage.getItem("symbol");
-    console.log(symbol);
+    //console.warn(symbol);
     if (symbol == null)
         arr1 = ["BTC"];
     else {
@@ -42,14 +41,12 @@ async function getIndex1() {
     }
 
 
-
-
     const res = await fetch("https://api.attlas.io/api/v1/futures/market_watch");
     const record = await res.json();
     var data = record.data;
     var rest = data.filter(item => arr1.includes(item.b));
     rest.sort();
-    //console.log(rest);
+    //console.warn(rest);
 
     const result = rest.reduce(function (r, a) {
         r[a.b] = r[a.b] || [];
@@ -57,19 +54,9 @@ async function getIndex1() {
         return r;
     }, Object.create(null));
 
-    //var group_to_values = rest.reduce(function (obj, item) {
-    //    obj[item.b] = obj[item.b] || [];
-    //    obj[item.b].push(item.p);
-    //    return obj;
-    //}, {});
-
-    //console.log(group_to_values);
-    //console.log(result);
-
-
     var groups = Object.keys(result).map(function (key) {
-        //console.log(result[key]);
-        return {
+        //console.warn(result[key]);
+        return {    
             symbol: key,
             usd: result[key][0].p,
             usd_ph: result[key][0].ph,
@@ -92,7 +79,7 @@ async function getIndex1() {
             vnd_all: result[key][1],
         };
     });
-    console.log(groups);
+    //console.log(groups);
 
     var para = "";
     for (var i = 0; i < groups.length; i++) {
@@ -107,8 +94,6 @@ async function getIndex1() {
             compare = "-";
             percent = ((groups[i].usd_ld - groups[i].usd) / groups[i].usd_ld * 100).toFixed(2);
         }
-
-
 
 
         para += `<tr>`;
@@ -150,7 +135,127 @@ async function getIndex1() {
     }
     const element = document.getElementById("data");
     element.innerHTML = para;
+
+    //senMessage();
 }
+
+
+
+
+async function GetCoinGood() {
+
+    const res = await fetch("https://api.attlas.io/api/v1/futures/market_watch");
+    const record = await res.json();
+    var rest = record.data;
+    rest.sort();
+    //console.warn(rest);
+
+    const result = rest.reduce(function (r, a) {
+        r[a.b] = r[a.b] || [];
+        r[a.b].push(a);
+        return r;
+    }, Object.create(null));
+
+    var groups = Object.keys(result).map(function (key) {
+        //console.warn(result[key]);
+        return {
+            symbol: key,
+            usd: result[key][0].p,
+            usd_ph: result[key][0].ph,
+            usd_pw: result[key][0].pw,
+            usd_p1m: result[key][0].p1m,
+            usd_p3m: result[key][0].p3m,
+            usd_h: result[key][0].h,
+            usd_lh: result[key][0].lh,
+            usd_ld: result[key][0].ld,
+            usd_all: result[key][0],
+
+            vnd: result[key][1].p,
+            vnd_ph: result[key][1].ph,
+            vnd_pw: result[key][1].pw,
+            vnd_p1m: result[key][1].p1m,
+            vnd_p3m: result[key][1].p3m,
+            vnd_h: result[key][1].h,
+            vnd_lh: result[key][1].lh,
+            vnd_ld: result[key][1].ld,
+            vnd_all: result[key][1],
+        };
+    });
+    console.warn(groups);
+
+
+    var arr = [];
+
+    var para = "";
+    for (var i = 0; i < groups.length; i++) {
+
+        var percent = 0;
+        var compare = "";
+        if (groups[i].usd >= groups[i].usd_ld) {
+            compare = "+";
+            percent = ((groups[i].usd - groups[i].usd_ld) / groups[i].usd_ld * 100).toFixed(2);
+        }
+        else {
+            compare = "-";
+            percent = ((groups[i].usd_ld - groups[i].usd) / groups[i].usd_ld * 100).toFixed(2);
+        }
+        groups[i].percent = percent;
+        groups[i].compare = compare;
+    }
+
+    var topValues = groups.sort((a, b) => b.percent - a.percent).slice(1, 3);
+    console.log(topValues);  
+
+    var para = "";
+    for (var i = 0; i < topValues.length; i++) {
+
+        para += `<tr>`;
+        para += "<td style='position:relative' >"
+            + "<a  target='_blank' href='" + getHref(topValues[i].vnd_all.s) + "' >" + topValues[i].symbol + "</a>"
+
+            + "<span style='position:absolute; top:1px; margin-left:2px;  font-size:11px; color: " + (topValues[i].usd >= topValues[i].usd_p1m ? " #3ab829;" : "#fd254d;") + " '>" + new Intl.NumberFormat("de-DE").format(topValues[i].usd_p1m) + " (" + new Intl.NumberFormat("de-DE").format(topValues[i].vnd_p1m) + ")" + "</span>"
+            + "<span style='position:absolute; top:15px; margin-left:2px;  font-size:11px; color: " + (topValues[i].usd >= topValues[i].usd_p3m ? " #3ab829;" : "#fd254d;") + " '>" + new Intl.NumberFormat("de-DE").format(topValues[i].usd_p3m) + " (" + new Intl.NumberFormat("de-DE").format(topValues[i].vnd_p3m) + ")" + "</span>"
+
+            + "<span style='position:absolute; top:1px; right:0; font-size:14px; color: " + (topValues[i].compare == "+" ? " #3ab829;" : "#fd254d;") + " '>" + topValues[i].compare + topValues[i].percent + "%" + "</span>"
+            + "<span style='position:absolute; bottom:0; right:0; font-size:14px; color: " + (topValues[i].usd >= topValues[i].usd_pw ? " #3ab829;" : "#fd254d;") + " '>" + new Intl.NumberFormat("de-DE").format(topValues[i].usd_pw) + "</span>"
+
+        para += "</td>";
+
+
+        para += "<td style='position:relative' >"
+            + new Intl.NumberFormat("de-DE").format(topValues[i].usd)
+
+            + "<span style='position:absolute; top:1px; margin-left:2px;  font-size:11px; color: #3ab829;'>" + new Intl.NumberFormat("de-DE").format(topValues[i].usd_h) + "</span>"
+            + "<span style='position:absolute; top:15px; margin-left:2px;  font-size:11px; color: #fd254d;'>" + new Intl.NumberFormat("de-DE").format(topValues[i].usd_lh) + "</span>"
+
+            + "<span style='position:absolute; bottom:0; right:0; font-size:14px;  color:  " + (topValues[i].usd >= topValues[i].usd_ph ? " #3ab829;" : "#fd254d;") + " '>" + new Intl.NumberFormat("de-DE").format(topValues[i].usd_ph) + "</span>"
+
+        para += "</td>";
+
+        para += "<td style='position:relative' >"
+            + new Intl.NumberFormat("de-DE").format(topValues[i].vnd)
+
+            + "<span style='position:absolute; top:1px; margin-left:2px;  font-size:11px; color: #3ab829;'>" + new Intl.NumberFormat("de-DE").format(topValues[i].vnd_h) + "</span>"
+            + "<span style='position:absolute; top:15px; margin-left:2px;  font-size:11px; color: #fd254d;'>" + new Intl.NumberFormat("de-DE").format(topValues[i].vnd_lh) + "</span>"
+
+            + "<span style='position:absolute; bottom:0; right:0; font-size:14px; color:" + (topValues[i].vnd >= topValues[i].vnd_ph ? " #3ab829;" : "#fd254d;") + " '>" + new Intl.NumberFormat("de-DE").format(topValues[i].vnd_ph) + "</span>"
+
+        para += "</td>";
+
+
+
+        para += `</tr>`;
+    }
+    const element = document.getElementById("data2");
+    element.innerHTML = para;
+
+    //senMessage();
+}
+
+
+
+
+
 
 function dateToDDMMYYYHHMMSS(date) {
     var d = date.getDate();
@@ -162,8 +267,6 @@ function dateToDDMMYYYHHMMSS(date) {
     var ss = date.getSeconds();
     return '' + (d <= 9 ? '0' + d : d) + '-' + (m <= 9 ? '0' + m : m) + '-' + y + "  " + (hh <= 9 ? '0' + hh : hh) + ":" + (mm <= 9 ? '0' + mm : mm) + ":" + (ss <= 9 ? '0' + ss : ss);
 }
-
-
 function onShowHideModal() {
     var x = document.getElementById("myModal");
     if (x.style.display === "none") {
@@ -178,11 +281,6 @@ function onShowHideModal() {
         document.getElementById("name").value = symbol;
     }
 }
-
-
-
-
-
 function onConfrim() {
     const symbol_get = document.getElementById("name").value;
     localStorage.setItem("symbol", symbol_get);
@@ -190,7 +288,6 @@ function onConfrim() {
     var x = document.getElementById("myModal");
     x.style.display = "none";
 }
-
 
 var input = document.getElementById("name");
 input.addEventListener("keypress", function (event) {
@@ -201,49 +298,34 @@ input.addEventListener("keypress", function (event) {
     }
 });
 
+async function senMessage() {
+    try {
+        var mess = "Đây là con bot tự động gửi từ huy";
+        var requestOptions = {
+            method: 'POST',
+            redirect: 'follow'
+        };
+        fetch("https://api.telegram.org/bot6514337909:AAGl5ZmX_Fi7yXcJgnSPGQd6xaXUpDCYVBI/sendMessage?chat_id=1062521039&text= " + mess, requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    } catch (error) {
+        console.error(error);
+    }
+}
+async function senMessage1() {
+    try {
+        var mess = "Đây là con bot tự động gửi từ huy";
+        var settings = {
+            "url": "https://api.telegram.org/bot6514337909:AAGl5ZmX_Fi7yXcJgnSPGQd6xaXUpDCYVBI/sendMessage?chat_id=1062521039&text=" + mess,
+            "method": "POST",
+            "timeout": 0,
+        };
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-
-
-//async function getIndex1() {
-//    try {
-//        const response = await axios.get('https://api.attlas.io/api/v1/futures/mark_price?baseAssetId=833&quoteAssetId=617');
-
-//        console.log(response.data);
-//        var data = response.data.data;
-
-//        document.getElementById("Symbol").innerHTML = data.symbol;
-//        document.getElementById("BaseAsset").innerHTML = data.baseAsset;
-//        document.getElementById("MarkPrice").innerHTML = new Intl.NumberFormat("de-DE").format(data.markPrice);
-//        document.getElementById("IndexPrice").innerHTML = new Intl.NumberFormat("de-DE").format(data.indexPrice);
-//        document.getElementById("FundingRate").innerHTML = data.fundingRate;
-//        document.getElementById("EventTime").innerHTML = moment(new Date(data.eventTime)).format('DD/MM/YYYY HH:mm:ss');
-//    } catch (error) {
-//        console.error(error);
-//    }
-//}
-
-//axios.post('/Contractor/Post', this.row).then(response => {
-//    this.data = response.data.items;
-//    $('.loader').delay(100).hide(0);
-//}).catch(error => {
-//    $('.loader').delay(100).hide(0);
-//    console.log(error);
-//});
-
-//var modal = document.getElementById("myModal");
-//var btn = document.getElementById("myBtn");
-//var span = document.getElementsByClassName("close")[0];
-
-//btn.onclick = function () {
-//    modal.style.display = "block";
-//}
-
-//span.onclick = function () {
-//    modal.style.display = "none";
-//}
-
-//window.onclick = function (event) {
-//    if (event.target == modal) {
-//        modal.style.display = "none";
-//    }
-//}
